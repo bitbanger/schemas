95875c438521bc646d67fcd3a11d01617f98a0e4
Lane <lanelawley@gmail.com>
2019-02-28 12:20:23 -0500

    First crack at a manually-translated fishing story in EL

--- stories.lisp
+++ stories.lisp
@@ -65,3 +65,101 @@
 
 )
 )
+
+
+(defparameter fishing-1
+'(
+
+	; Boys like to catch fish.
+	(
+		(E1.SK AT-ABOUT.P NOW0)
+
+		((ALL B (B BOY.N)
+			((B LIKE.V (KA (CATCH.V (K (PLUR FISH.N))))))) ** E1.SK)
+	)
+
+	; It is a good sport.
+	(
+		(E2.SK AT-ABOUT.P NOW1)
+
+		((IT.PRO (GOOD.A SPORT.N)) ** E2.SK)
+	)
+
+	; Here is Tom with his rod and line.
+	(
+		(E3.SK AT-ABOUT.P NOW2)
+
+		(ROD1.SK ROD.N)
+		(LINE1.SK LINE.N)
+		(ROD1.SK PERTAIN-TO TOM.NAME)
+		(LINE1.SK PERTAIN-TO TOM.NAME)
+		(TOM.NAME WITH.P ROD1.SK)
+		(TOM.NAME WITH.P LINE1.SK)
+
+		; Unclear (to me) whether this is a copula
+		; or an existential "be", but it's useful
+		; as a characterizer regardless, even if it's
+		; only a "dummy" characterizer.
+		((TOM.NAME BE.V) ** E3.SK)
+	)
+
+	; A hook is on the end of the line.
+	(
+		(E4.SK AT-ABOUT.P NOW3)
+
+		(HOOK1.SK HOOK.N)
+		(LINE2.SK LINE.N)
+		(END1.SK END.N)
+		(END1.SK PERTAIN-TO LINE2.SK)
+
+		((HOOK1.SK ON.P END1.SK) ** E4.SK)
+	)
+
+	; He has a bag, too.
+	(
+		(E5.SK AT-ABOUT.P NOW4)
+
+		(BAG1.SK BAG.N)
+
+		((HE.PRO (TOO.ADV-A (HAVE.V BAG1.SK))) ** E5.SK)
+	)
+
+	; The bag is to put the fish in.
+	(
+		(E6.SK AT-ABOUT.P NOW5)
+
+		(BAG2.SK BAG.N)
+		(FISH1.SK (PLUR FISH.N))
+
+		(((SUB BAG2.SK (((ADV-A (FOR.P (KA ((IN.P 
+
+		((BAG2.SK FOR.P (KA ((IN.P BAG2.SK) PUT.V FISH1.SK))) ** E6.SK)
+	)
+
+	; Here is the fish for Tom to catch.
+	(
+		(E7.SK AT-ABOUT.P NOW6)
+
+		(FISH2.SK FISH.N)
+
+		((FISH2.SK ((FOR.P (KA (TOM.NAME CATCH.V FISH2.SK))) BE.V)) ** E7.SK)
+	)
+
+	; It swims with its tail.
+	(
+		(E8.SK AT-ABOUT.P NOW7)
+
+		(TAIL1.SK TAIL.N)
+		(TAIL1.SK PERTAIN-TO IT.PRO)
+
+		((IT.PRO ((WITH.P TAIL1.SK) SWIM.V)) ** E8.SK)
+	)
+
+	; It can swim very fast.
+	(
+		(E9.SK AT-ABOUT.P NOW8)
+
+		((IT.PRO (CAN.MD ((ADV-A (VERY.ADV-A FAST.A)) SWIM.V))) ** E9.SK)
+	)
+)
+)

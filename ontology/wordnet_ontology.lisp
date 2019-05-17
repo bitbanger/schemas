cfecaefdf02ee07ec8bf7c9ca7f695a98c6ad701
bkane2 <Ben.kane@me.com>
2019-05-17 15:49:17 -0400

    Added ontology folder

---
+++ ontology/wordnet_ontology.lisp
@@ -0,0 +1,5 @@
+; (defun wordnet-hypernyms (word) ... )
+;; give a list like (word X Y Z ... ENTITY), where it's the whole "ladder" of hypernyms up to ENTITY
+
+; (defun wordnet-common-hypernym (word1 word2) ... )
+;; take the intersection of both ladders for each word

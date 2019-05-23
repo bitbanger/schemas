5bbd9189c3edd8367125181c6b1f91693e029333
Lane <lanelawley@gmail.com>
2019-05-23 13:02:28 +0200

    Better term unification/generalization

--- story_unify.lisp
+++ story_unify.lisp
@@ -1,9 +1,7 @@
-(load "wnlisp/packages.lisp")
-(use-package 'wordnet)
-
 (load "stories.lisp")
 (load "parse.lisp")
 (load "real_util.lisp")
+(load "ontology/wordnet-ontology.lisp")
 
 (defun generalizable-term? (x)
 (and
@@ -127,7 +125,7 @@
 			; (that is, skolemize them)
 			;(format t "	~s:~%" (gethash ut uts))
 			(setf (gethash this-term-var term-constrs)
-				(loop for vc in (second (term-var-constraints ut fishing_green))
+				(loop for vc in (second (term-var-constraints ut story))
 					;do (format t "		~s~%" (list (gethash ut uts) (second vc)))
 					collect (list this-term-var (second vc))
 			))
@@ -138,20 +136,151 @@
 )
 )
 
+(defun plur? (term)
+(and
+	(listp term)
+	(equal 2 (length term))
+	(equal (car term) 'PLUR)
+)
+)
+
+(defun norm-singletons (x)
+	(if (listp x)
+		x
+		; else
+		(list x)
+	)
+)
+
+(defun last-of (x)
+	(car (last (norm-singletons x)))
+)
+
+(defun safe-length (x)
+	(if (listp x)
+		(length x)
+		; else
+		1
+	)
+)
+
+(defun resolve-as (phi) phi)
+
+(defun resolve-as-plur (phi)
+	(list 'PLUR phi)
+)
+
+(defun with-certainty (cert resolver st1 st2)
+	(loop for pair in (resolve-terms st1 st2)
+		collect (list
+				(* cert (car pair))
+				(funcall resolver (second pair))
+			)
+	)
+)
+
+(defun resolve-terms (t1 t2)
+(cond
+	; Plurals are certain if both terms are plural
+	((and (plur? t1) (plur? t2))
+		; conservative
+		(append
+			(with-certainty 1.0 #'resolve-as-plur (second t1) (second t2))
+		)
+	)
+
+	; Remove plurals & add the plural unifier as an uncertain term
+	((plur? t1)
+		; conservative
+		(append
+			(with-certainty 1.0 #'resolve-as (second t1) t2)
+
+			; OPT: cache this second call, here & elsehwre
+			(with-certainty 0.5 #'resolve-as-plur (second t1) t2)
+		)
+	)
+
+	((plur? t2)
+		; conservative
+		(append
+			(with-certainty 1.0 #'resolve-as t1 (second t2))
+
+			; OPT: cache this second call, here & elsehwre
+			(with-certainty 0.5 #'resolve-as-plur t1 (second t2))
+		)
+	)
+
+	; Unify the last terms in compound predicates (bc they'll be nouns?)
+	((or (> (safe-length t1) 1) (> (safe-length t2) 1))
+		(append
+			; conservative
+			(with-certainty 1.0 #'resolve-as (last-of t1) (last-of t2))
+		)
+	)
+
+	; LCH for symbols
+	((and (symbolp t1) (symbolp t2))
+		; conservative
+		(append
+			(list (list 1.0 (wordnet-common-hypernym t1 t2)))
+		)
+	)
+
+	(t nil)
+)
+)
+
+(defun interesting? (term)
+	(not (or
+		(equal 'ENTITY.N term)
+		(equal '(PLUR ENTITY.N) term)
+	))
+)
 
 
 
+
+(defparameter gsr (extract-story-roles fishing_green))
+(defparameter bsr (extract-story-roles fishing_blue))
 
 
 (format t "~%GREEN:~%")
-(print-ht (extract-story-roles fishing_green))
+(print-ht gsr)
 
 (format t "~%BLUE:~%")
-(print-ht (extract-story-roles fishing_blue))
+(print-ht bsr)
+
+(loop for gkey being the hash-keys of gsr do
+(loop for gp in (gethash gkey gsr) do
+(loop for bkey being the hash-keys of bsr do
+(loop for bp in (gethash bkey bsr) do (block inner
+	(setf gprop (second gp))
+	(setf bprop (second bp))
+	(setf resprop (resolve-terms gprop bprop))
+
+	(block check_interestingness
+		(loop for rp in resprop do
+			(if (interesting? (second rp))
+				(return-from check_interestingness))
+		)
+
+		; If we only saw "entity", forget this unification
+		(return-from inner)
+	)
+
+	(format t "(~s, ~s) ~s & ~s =~%" gkey bkey gprop bprop)
+	(loop for rp in resprop do (block innermost
+		(if (not (interesting? (second rp))) (return-from innermost))
+
+		(format t "	~s~%" rp)
+	))
+	(format t "~%")
+)))))
 
 
 
 
+#|
 (format t "~%OLD:~%")
 (defparameter blue-uts (make-hash-table :test #'equal))
 (loop for ut in (unique-terms fishing_blue)
@@ -171,3 +300,4 @@
 		)
 	)
 )
+|#

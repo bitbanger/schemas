d21ec5723552752d672e0251f338ebe96f6254ee
Lane <lanelawley@gmail.com>
2019-06-25 12:43:47 -0400

    Partial coreference working

--- coref.lisp
+++ coref.lisp
@@ -16,11 +16,25 @@
 	)
 )
 
+(defun all-prop-args (prop)
+	(loop for e in prop
+		if (and (term? e) (not (adv? e)))
+			collect e
+	)
+)
+
+(defun prop-mods (prop)
+	(loop for e in prop
+		if (adv? e)
+			collect e
+	)
+)
+
 ; Determine whether a term needs coreference
 (defun coref? (term)
 (or
 	(lex-pronoun? term)
-	(gethash (list term 'THE.A) *KB*)
+	(eval-prop (list term 'THE.A))
 )
 )
 
@@ -36,14 +50,86 @@
 
 ; Get all predicates that apply to a term
 (defun get-term-preds (term)
+(append
 	(loop for prop in (gethash term *KB-ARG-IND*)
 		if (not (null (get-pred prop)))
 			collect (get-pred prop)
 	)
+
+	(loop for pred in *IRREGULAR-PREDS*
+		if (eval-prop (list term pred))
+			collect pred
+	)
+)
+)
+
+; Get all terms to which a predicate applies
+(defun get-pred-terms (pred)
+	; "Irregular" predicates aren't evaluated using the KB
+	(if (not (irregular-pred? pred))
+		(gethash pred *KB-PRED-IND*)
+
+		; else
+		(loop for term being the hash-keys of *KB-ARG-IND*
+			if (eval-prop (list term pred))
+				collect term
+		)
+	)
+)
+
+; Evaluate whether a proposition is true in the knowledge base
+(defun eval-prop (prop)
+(block outer
+	(setf pred (get-pred prop))
+	(setf args (all-prop-args prop))
+	(setf mods (prop-mods prop))
+
+	; (format t "raw proposition: ~s~%" prop)
+	; (format t "canonical proposition: ~s(" pred)
+	;(loop for arg in args
+	;	do (format t " ~s " arg)
+	;)
+	;(format t ")~%")
+	;(if (> (length mods) 0) (block print-mods
+	;	(format t "	prop modifiers:~%")
+	;	(loop for mod in mods
+	;		do (format t "		~s~%" mod)
+	;	)
+	;))
+
+	(if (equal 1 (length args)) (block monadic-special-cases
+		(setf arg (car args))
+
+		; Names refer to agents.
+		(if (and (symbolp arg) (has-suffix? (string arg) ".NAME"))
+			(if (equal pred 'AGENT1.N)
+				(return-from outer t))
+		)
+
+		; He and she pronouns refer to agents.
+		; TODO: handle "they"
+		(if (and (symbolp arg) (or (equal arg 'HE.PRO) (equal arg 'SHE.PRO)))
+			(if (equal pred 'AGENT1.N)
+				(return-from outer t))
+		)
+	))
+
+	(if (gethash prop *KB*)
+		(return-from outer t)
+	)
+)
 )
 
 (defun safe-inc (n)
 	(if (null n) 1 (+ n 1))
+)
+
+(defparameter *IRREGULAR-PREDS* '(
+	AGENT1.N
+))
+
+(defun irregular-pred? (pred)
+	(not (null (member pred *IRREGULAR-PREDS*)))
 )
 
 
@@ -84,7 +170,7 @@
 				(loop for pred in (get-term-preds e)
 					if (not (equal pred 'THE.A))
 						; do (format t "~s: ~s~%" pred (gethash pred *KB-PRED-IND*))
-						append (loop for term in (gethash pred *KB-PRED-IND*)
+						append (loop for term in (get-pred-terms pred)
 							if (not (equal term e))
 								; collect term
 								do (setf (gethash term share-count) (safe-inc (gethash term share-count)))
@@ -94,7 +180,7 @@
 				(setf max-count 0)
 				(setf max-coref nil)
 				(loop for term being the hash-keys of share-count
-					if (> (gethash term share-count) max-count)
+					if (>= (gethash term share-count) max-count)
 						do (block update
 							(setf max-count (gethash term share-count))
 							(setf max-coref term)
@@ -108,3 +194,5 @@
 )
 
 
+
+(eval-prop '(MAY.NAME EAT.V (K BALL.N) (ADV-A (WITH.P (K GUSTO.N)))))

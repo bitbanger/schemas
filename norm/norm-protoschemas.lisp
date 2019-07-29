f4ae4cf3f8fd9a2f01b6b1afe85ab9aa2acf23ef
Lane <lanelawley@gmail.com>
2019-07-29 15:02:48 -0400

    Protoschemas

--- norm/norm-protoschemas.lisp
+++ norm/norm-protoschemas.lisp
@@ -1,3 +1,57 @@
+(defparameter do_action_for_pleasure.v
+	'(epi-schema ((?x do_action_for_pleasure.v ?a) ** ?e)
+		(:Roles
+			(!r1 (?x agent1.n))
+			(!r2 (?a action1.n))
+			(!r3 (?p pleasure.n))
+		)
+
+		(:Goals
+			(?g1 (?x (want.v (ka (experience.v ?p)))))
+		)
+
+		(:Steps
+			(?e1 (?x (do.v ?a)))
+		)
+
+		(:Postconds
+			(!p1 (?x (experience.v ?p)))
+		)
+
+		(:Episode-relations
+			(!w1 (?g1 cause.v ?e1))
+			(!w2 (?e1 cause.v ?p1))
+		)
+	)
+)
+
+(defparameter avoid_action_to_avoid_displeasure.v
+	'(epi-schema ((?x avoid_action_to_avoid_displeasure.v ?a) ** ?e)
+		(:Roles
+			(!r1 (?x agent1.n))
+			(!r2 (?a action1.n))
+			(!r3 (?d displeasure.n))
+		)
+
+		(:Goals
+			(?g1 (?x (want.v (that (not (?x (experience.v ?d)))))))
+		)
+
+		(:Preconds
+			(!i1 (if (?x (do.v ?a))
+						(?x (experience.v ?d))))
+		)
+
+		(:Steps
+			(?e1 (not (?x (do.v ?a))))
+		)
+
+		(:Episode-relations
+			(!w1 (?g1 cause.v ?e1))
+		)
+	)
+)
+
 (defparameter do_action_to_enable_action.v
 	'(epi-schema ((?x do_action_to_enable_action.v ?a1 ?a2) ** ?e)
 		(:Roles
@@ -21,6 +75,35 @@
 
 		(:Episode-relations
 			(!w1 (?e1 cause.v ?e2))
+			(!w2 (?g1 cause.v ?e1))
+		)
+	)
+)
+
+(defparameter give_object.v
+	'(epi-schema ((?x give_object.v ?o (to.p-arg ?y)) ** ?e)
+		(:Roles
+			(!r1 (?x agent1.n))
+			(!r2 (?o object1.n))
+			(!r3 (?y agent1.n))
+		)
+
+		(:Goals
+			(?g1 (?x (want.v (that (?y (have.v ?o))))))
+		)
+
+		(:Preconds
+			(!i1 (?x have.v ?o))
+			(!i1 (not (?y have.v ?o)))
+		)
+
+		(:Steps
+			(?e1 (?x (give.v ?o (to.p-arg ?y))))
+		)
+
+		(:Postconds
+			(!p1 (not (?x have.v ?o)))
+			(!p2 (?y have.v ?o))
 		)
 	)
 )

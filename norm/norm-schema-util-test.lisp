5510fbac0fda9b9ff9a8b577a720984d6f0a1fab
Lane <lanelawley@gmail.com>
2019-08-12 11:31:49 -0400

    Beginning movements for refactoring

--- norm/norm-schema-util-test.lisp
+++ norm/norm-schema-util-test.lisp
@@ -4,133 +4,13 @@
 (load "norm-protoschemas.lisp")
 (load "norm-stories.lisp")
 (load "norm-unify.lisp")
-(load "norm-generalize.lisp")
+(load "norm-match.lisp")
 
-; TODO: check constraints
-(defun unify-with-schema (phi schema story)
-(let (bindings)
-(block outer
-	(loop for sec in (schema-sections schema)
-		do (format t "	schema sec ~s~%" (section-name sec))
-		do (loop for formula in (section-formulas sec) do
-			(block uni
-				(format t "		schema ~s~%" (second formula))
-				;(format t "	~s~%" (second formula))
-				(setf new-bindings (unify (second formula) phi bindings))
-				; (format t "set bindings are ~s~%" (ht-to-str bindings))
-				; (setf (gethash bindings (car formula)) 
-				(if (null new-bindings)
-					; then
-					;(format t "no uni~%")
-					;(return-from outer nil)
-					nil
-					; else
-					(progn
-					(format t "bound to ~s~%" (second formula))
-					(format t "bindings are ~s~%" new-bindings)
-					(if (canon-charstar? phi)
-						; then
-						; (format t "temporal formula ~s <-> ~s~%" (third phi) (car formula))
-						(if (bind-if-unbound (car formula) (third phi) new-bindings)
-							; then
-							(return-from outer new-bindings)
-							; (setf bindings new-bindings)
-							; else
-							(format t "~s already bound; cannot bind!~%" (car formula))
-						)
-
-						; else
-						(return-from outer new-bindings)
-					)
-					;(format t "UNIFIED~%")
-					;(format t "	~s~%" formula)
-					; (format t "WITH~%")
-					; (format t "	~s~%" phi)
-					;(format t "bindings:~%~s~%" (ht-to-str bindings))
-					;(format t "small individuals:~%~s~%" (extract-small-individuals phi))
-					;(format t "extra constraints:~%~s~%" (story-select-term-constraints story (extract-small-individuals phi)))
-					;(format t "final schema:~%~s~%" (apply-bindings schema bindings))
-					;(format t "final schema with added constraint:~%~s~%" (add-role-constraint (apply-bindings schema bindings) 'TRUE))
-
-					(return-from outer bindings)
-					)
-				)
-			)
-		)
-	)
-
-	(return-from outer bindings)
-	)
-))
 
 
 
 ; (setf test-story *FLOWER-STORY*)
 (setf test-story *KITE-STORY*)
-
-; TODO: for each bound individual (& all its sub-individuals),
-; extract all relevant props from story. This code exists in
-; coref.lisp. Then figure out a way to add them to the schema
-; as additional constraints.
-(defun match-story-to-schema (test-story in-schema generalize)
-(block outer
-	(setf test-schema in-schema)
-
-	(loop for sent in test-story do
-		(loop for phi in sent do (block uni-loop
-	
-			(setf small-inds (extract-small-individuals phi))
-			(setf constraints (story-select-term-constraints test-story small-inds))
-	
-			(format t "trying:~%")
-			(format t "	formula ~s~%" phi)
-			(setf go-match (unify-with-schema phi test-schema test-story))
-	
-			(if (not (null go-match))
-				; then
-				(block print-go-match
-					(format t "bound to story formula ~s~%" phi)
-					; (format t "Extra constraints: ~s~%" constraints)
-	
-					(setf go-match-schema (apply-bindings test-schema go-match))
-	
-					; Make sure the full matched sentence goes in the step slot
-					(setf new-steps (list ':Steps))
-					(loop for s in (cdr (get-section go-match-schema ':Steps)) do (block fix-steps
-						(if (and (canon-charstar? phi) (equal (third phi) (car s)))
-							; then
-							(setf new-steps (append new-steps (list (list (car s) (car phi)))))
-							; else
-							(setf new-steps (append new-steps (list s)))
-					)))
-					(setf go-match-schema (set-section go-match-schema ':Steps new-steps))
-	
-					(loop for const in constraints do (block const-add
-						(setf go-match-schema (add-role-constraint go-match-schema const))
-					))
-					; Now replace terms by variables
-					(if generalize
-						(progn
-						(format t "generalizing~%")
-						(setf gen-cursor "?_A")
-						(loop for ind in small-inds do (block gen-ind-loop
-							(setf go-match-schema (replace-vals ind (intern gen-cursor) go-match-schema))
-							(setf gen-cursor (next-str gen-cursor))
-						))
-						)
-					)
-					; (format t "test-schema match + generalization:~%~s~%" "")
-					; (print-schema go-match-schema)
-					; (return-from outer go-match-schema)
-					(setf test-schema go-match-schema)
-				)
-			)
-		))
-	)
-
-	(return-from outer test-schema)
-)
-)
 
 (setf kite-gen-schema (match-story-to-schema *KITE-STORY* go_somewhere.v t))
 (print-schema kite-gen-schema)
--- norm/norm-schema-util.lisp
+++ norm/norm-schema-util.lisp
@@ -1,5 +1,6 @@
 (load "norm-el.lisp")
 (load "real_util.lisp")
+(load "coref.lisp")
 
 (defparameter *SEC-NAMES* '(
 	:Roles
@@ -151,5 +152,67 @@
 		do (format t "	)~%")
 	)
 	(format t ")~%")
+)
+)
+
+(defun temporal-prop? (prop)
+	; TODO: more nuanced temporal proposition identification
+	(has-element prop '**)
+)
+
+(defun lambda-prop? (prop)
+	; TODO: more nuanced lambda proposition identification
+	(has-element prop 'LAMBDA.EL)
+)
+
+(defun story-term-constraints (story)
+(let (
+			(gen-kb (list (make-hash-table :test #'equal)
+						  (make-hash-table :test #'equal)
+						  (make-hash-table :test #'equal)))
+
+			(constraints (make-hash-table :test #'equal))
+	)
+(block outer
+	; Index the story into the knowledge base
+	(loop for sent in story do
+		(loop for wff in sent
+			do (add-to-kb wff gen-kb)))
+
+	; Reindex only small individuals by only non-lambda,
+	; non-temporal constraints
+	(loop for arg being the hash-keys of (kb-arg-ind gen-kb)
+		if (canon-small-individual? arg) do (block inner
+			(loop for pred in (gethash arg (kb-arg-ind gen-kb))
+				if (and
+						(not (temporal-prop? pred))
+						(not (lambda-prop? pred)))
+				; then
+				do (if (not (member pred (gethash arg constraints) :test #'equal))
+					; then
+					(push pred (gethash arg constraints))
+				)
+			)
+		)
+	)
+
+	; Return the constraints
+	(return-from outer constraints)
+)
+)
+)
+
+(defun story-select-term-constraints (story terms)
+(let ((constraints (story-term-constraints story)))
+	(remove-duplicates (loop for term being the hash-keys of constraints
+		if (member term terms :test #'equal)
+		append (gethash term constraints)) :test #'equal)
+)
+)
+
+(defun extract-small-individuals (phi)
+(block outer
+	; (format t "extracting from ~s~%" phi)
+	(return-from outer (get-elements-pred phi #'canon-small-individual?))
 )
 )

f48e2295fbc07f778b42c8aa8aa4ea07a46661d8
Lane Lawley <llawley@cs.rochester.edu>
2020-11-18 11:11:53 -0500

    Separated out a test file for some school-related ROCstories

---
+++ norm/school-roc-stories.lisp
@@ -0,0 +1,41 @@
+(defparameter *SCHOOL-ROC-STORIES* '(
+	(
+	"This is a schoolhouse in America."
+	"We can see some boys and girls in the street."
+	"The boys and girls are going to school."
+	"Last night they studied their lessons."
+	"This is a large, fine schoolhouse; it is made of stone."
+	)
+
+	(
+	"Today I went to school."
+	"It was the first day of school."
+	"I met all my new teachers."
+	"I got all of my homework."
+	"I went home from school."
+	)
+
+	(
+	"It was the girl's first day at school."
+	"She was very nervous."
+	"She went into the classroom."
+	"She saw her friend."
+	"She sat next to her friend."
+	)
+
+	(
+	"Lewis didn't do his homework."
+	"He had to go to summer school."
+	"He liked the teacher."
+	"They became good friends."
+	"Lewis learned a lot at summer school."
+	)
+
+	(
+	"The boy locked the cage."
+	"He left for school."
+	"He returned home."
+	"The cage was unlocked."
+	"He caught his brother playing with his bird."
+	)
+))

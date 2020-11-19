63397c90a2c66abcdf1ea47a27a278a9d6535694
Lane Lawley <llawley@cs.rochester.edu>
2020-11-19 11:30:45 -0500

    Even more cleanup

--- norm/norm-generalize.lisp
+++
@@ -1,8 +0,0 @@
-(load "ll-load.lisp")
-
-(ll-load "norm-stories.lisp")
-(ll-load "coref.lisp")
-
-
-; print all term constraints
-(print-ht (story-select-term-constraints *COUSIN-STORY* '(HENRY.NAME CITY1.SK HE.PRO)))

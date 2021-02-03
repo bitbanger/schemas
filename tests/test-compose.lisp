260aa49e089cd74b6ba2b4680e4f15e5f37f81f3
Lane Lawley <llawley@cs.rochester.edu>
2021-02-03 15:31:18 -0500

    Compose test now generates schemas for stories

--- tests/test-compose.lisp
+++ tests/test-compose.lisp
@@ -2,5 +2,15 @@
 
 (load "../ll-load.lisp")
 
+(load "../stories/roc-mcguffey-stories.lisp")
+
+(ll-load-superdir "schema-matcher.lisp")
+(ll-load-superdir "schema-matcher.lisp")
 (ll-load-superdir "schema-link.lisp")
 (ll-load-superdir "schema-util.lisp")
+
+(loop for roc-story in *ROC*
+	do (handler-case (loop for schema in (top-story-matches-easy roc-story)
+		do (print-schema (car schema))
+	) (error () (format t "story processing error~%")))
+)

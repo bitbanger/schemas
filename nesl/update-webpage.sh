66560f188c946f2b8195f793ce6697dd5d9dc489
bitbanger <lanelawley@gmail.com>
2021-12-14 00:20:14 -0500

    Fixed an invocation in update-webpage.sh

--- nesl/update-webpage.sh
+++ nesl/update-webpage.sh
@@ -4,7 +4,7 @@ cp nesl-compos.lisp old-nesl-compos.lisp
 
 tac old-nesl-compos.lisp | tail -n +2 | tac > nesl-compos.lisp
 
-python extract-composites.py nesl_output.txt >> nesl-compos.lisp
+python extract_composites.py nesl_output.txt >> nesl-compos.lisp
 
 echo '))' >> nesl-compos.lisp
 

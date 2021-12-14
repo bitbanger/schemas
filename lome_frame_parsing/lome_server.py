aeb1f6c0d28b588bbd06bb8274427f6521a11727
bitbanger <lanelawley@gmail.com>
2021-12-13 19:37:03 -0500

    LOME server class refactor

--- lome_frame_parsing/lome_server.py
+++ lome_frame_parsing/lome_server.py
@@ -16,72 +16,77 @@ INPUT_DIR = 'lome-server-input'
 OUTPUT_DIR = 'lome-server-output'
 ORIG_DIR = 'lome-server-orig'
 
-frame_lispifier = lispify_comm_frames.FrameLispifier()
+class LOMEParser:
+	def __init__(self):
+		self.frame_lispifier = lispify_comm_frames.FrameLispifier()
 
-def clear_dirs():
-	for d in [INPUT_DIR, OUTPUT_DIR, ORIG_DIR]:
-		subprocess.run(('rm -rf %s/%s' % (os.getcwd(), d)).split(' '))
-		subprocess.run(('mkdir %s/%s' % (os.getcwd(), d)).split(' '))
+	def clear_dirs(self):
+		for d in [INPUT_DIR, OUTPUT_DIR, ORIG_DIR]:
+			subprocess.run(('rm -rf %s/%s' % (os.getcwd(), d)).split(' '))
+			subprocess.run(('mkdir %s/%s' % (os.getcwd(), d)).split(' '))
 
-def run_lome():
-	subprocess.run(('docker run --gpus all --rm -v %s:/mnt %s /mnt/lome-server-input /mnt/lome-server-output' % (os.getcwd(), DOCKER_IMAGE_NAME)).split(' '))
+	def run_lome(self):
+		subprocess.run(('docker run --gpus all --rm -v %s:/mnt %s /mnt/lome-server-input /mnt/lome-server-output' % (os.getcwd(), DOCKER_IMAGE_NAME)).split(' '))
 
-def tokenize_story(story):
-	# Perform coreference resolution for now,
-	# since it's the only way I know to get
-	# the AllenAI coref tokenization.
-	# TODO: find out how to get it without
-	# actually doing coref analysis
-	resolved = coref.predict(story.strip())
+	def tokenize_story(self, story):
+		# Perform coreference resolution for now,
+		# since it's the only way I know to get
+		# the AllenAI coref tokenization.
+		# TODO: find out how to get it without
+		# actually doing coref analysis
+		resolved = coref.predict(story.strip())
 
-	# Get the tokenized sentences out
-	sents = ' '.join(resolved['document'])
+		# Get the tokenized sentences out
+		sents = ' '.join(resolved['document'])
 
-	# Strip each line
-	sents = '\n'.join([sent.strip() for sent in sents.split('\n')])
+		# Strip each line
+		sents = '\n'.join([sent.strip() for sent in sents.split('\n')])
 
-	return sents.strip()
+		return sents.strip()
 
-def write_story(story, story_num):
-	orig_filename = '%s/story%d.txt' % (ORIG_DIR, story_num)
-	tok_filename = '%s/story%d.txt' % (INPUT_DIR, story_num)
+	def write_story(self, story, story_num):
+		orig_filename = '%s/story%d.txt' % (ORIG_DIR, story_num)
+		tok_filename = '%s/story%d.txt' % (INPUT_DIR, story_num)
 
-	with open(orig_filename, 'w') as f:
-		f.write(story.strip())
+		with open(orig_filename, 'w') as f:
+			f.write(story.strip())
 
-	with open(tok_filename, 'w') as f:
-		f.write(tokenize_story(story))
+		with open(tok_filename, 'w') as f:
+			f.write(self.tokenize_story(story))
 
-def lispify_frames(stories):
-	comms = []
-	orig_txts = stories
+	def lispify_frames(self, stories):
+		comms = []
+		orig_txts = stories
 
-	for i in range(len(stories)):
-		comms.append(read_comm('%s/story%d.comm' % (OUTPUT_DIR, i)))
+		for i in range(len(stories)):
+			comms.append(read_comm('%s/story%d.comm' % (OUTPUT_DIR, i)))
 
-	pairs = [(comms[i], orig_txts[i]) for i in range(len(comms))]
+		pairs = [(comms[i], orig_txts[i]) for i in range(len(comms))]
 
-	return frame_lispifier.lispify_multiple_frames(pairs)
+		return self.frame_lispifier.lispify_multiple_frames(pairs)
 
-def parse_story_frames(stories):
-	clear_dirs()
+	def parse_story_frames(self, stories):
+		self.clear_dirs()
 
-	for i in range(len(stories)):
-		write_story(stories[i], i)
+		for i in range(len(stories)):
+			self.write_story(stories[i], i)
 
-	run_lome()
+		self.run_lome()
 
-	lisp = lispify_frames(stories)
+		lisp = self.lispify_frames(stories)
 
-	print(lisp)
-
-	return lisp
+		return lisp
 
 class RequestHandler(SimpleXMLRPCRequestHandler):
 	rpc_paths = ('/RPC2',)
 
 if __name__ == '__main__':
 	with SimpleXMLRPCServer(('localhost', 8040), requestHandler=RequestHandler) as server:
+		lome_parser = LOMEParser()
+
+		def parse_story_frames(stories):
+			return lome_parser.parse_story_frames(stories)
+
 		server.register_introspection_functions()
 
 		server.register_function(parse_story_frames)

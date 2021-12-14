3cb61db15e18ef5c3a5e94d79167357c98667537
bitbanger <lanelawley@gmail.com>
2021-12-13 22:09:21 -0500

    Fixed directories in update-webpage.sh

--- nesl/update-webpage.sh
+++ nesl/update-webpage.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
-cp ~/Code/schemas/tests/nesl-compos.lisp ~/Code/schemas/tests/old-nesl-compos.lisp
+cp nesl-compos.lisp old-nesl-compos.lisp
 
-tac ~/Code/schemas/tests/old-nesl-compos.lisp | tail -n +2 | tac > ~/Code/schemas/tests/nesl-compos.lisp
+tac old-nesl-compos.lisp | tail -n +2 | tac > nesl-compos.lisp
 
-cd ~/Code/lome
-python extract-composites.py nesl_output.txt >> ~/Code/schemas/tests/nesl-compos.lisp
+python extract-composites.py nesl_output.txt >> nesl-compos.lisp
 
-echo '))' >> ~/Code/schemas/tests/nesl-compos.lisp
-
-cd ~/Code/schemas/tests
+echo '))' >> nesl-compos.lisp
 
 sbcl --script make-nesl-webpage.lisp
 

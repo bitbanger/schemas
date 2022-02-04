b1de815812b3ac4139e28f61d281209e1c474ead
bitbanger <lanelawley@gmail.com>
2022-02-04 17:01:02 -0500

    Add

--- pyschemas/schema-webserver/test_schema_html.py
+++ pyschemas/schema-webserver/test_schema_html.py
@@ -1,3 +1,3 @@
 import schema_html
 
-print(schema_html.schemas_html('../../nesl/standalone-schemas/family_members_arguing_0.txt'))
+print(schema_html.schemas_html('standalone-schemas/family_members_arguing_0.txt'))

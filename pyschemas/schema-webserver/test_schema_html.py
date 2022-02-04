5dac9b4171ce6dbe8f336471738568d03e91198e
bitbanger <lanelawley@gmail.com>
2022-02-04 16:15:35 -0500

    Added webserver folder with barebones code to render schemas as HTML

---
+++ pyschemas/schema-webserver/test_schema_html.py
@@ -0,0 +1,3 @@
+import schema_html
+
+print(schema_html.schemas_html('../../nesl/standalone-schemas/family_members_arguing_0.txt'))
--- pyschemas/schema_html.py
+++
@@ -1,5 +0,0 @@
-import subprocess
-
-def schemas_html(schemas_filename):
-	with open(schemas_file, 'r') as f:
-		return subprocess.run('sbcl --script render-schemas.lisp'.split(' '), input=bytes(f.read(), 'utf-8'), cwd='..', capture_output=True).stdout.decode('utf-8')

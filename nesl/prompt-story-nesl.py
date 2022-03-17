9cb940e6677ca68e23bad0dbc2906f508532e12c
bitbanger <lanelawley@gmail.com>
2022-03-17 18:51:47 -0400

    Updates to NESL prompt-gen script

--- nesl/prompt-story-nesl.py
+++ nesl/prompt-story-nesl.py
@@ -10,8 +10,8 @@ from extract_composites import extract_compos
 
 from collections import defaultdict
 
-PROMPT = 'going to school'
-NUM_STORIES = 10
+PROMPT = 'going outside in the wintertime'
+NUM_STORIES = 15
 
 US_PROMPT = '_'.join(PROMPT.split(' '))
 
@@ -42,59 +42,93 @@ for story in stories_by_topic[topic]:
 	story_txts.append(load_story(story))
 '''
 
-story_txts = []
+def gen(prompt, num):
+	story_txts = []
+	us_prompt = '_'.join(prompt.split(' '))
 
-story_gen_start = time.time()
-print('\tgenerating %d %s...' % (NUM_STORIES, 'stories' if NUM_STORIES != 1 else 'story'), flush=True)
-for i in range(NUM_STORIES):
-	story = make_topical_stories(PROMPT)[0]
-	story_txts.append(story)
-	print('%d / %d' % (i+1, NUM_STORIES), flush=True)
-print('\tdone (%s)' % (fmt_time(story_gen_start, time.time())), flush=True)
+	story_gen_start = time.time()
+	print('\tgenerating %d %s...' % (num, 'stories' if num != 1 else 'story'), flush=True)
+	for i in range(num):
+		story = make_topical_stories(prompt)[0]
+		story_txts.append(story)
+		print('%d / %d' % (i+1, num), flush=True)
+	print('\tdone (%s)' % (fmt_time(story_gen_start, time.time())), flush=True)
 
-print('\trunning LOME...', flush=True)
-lome_start = time.time()
-# Run LOME and output the Lisp frames to a file
-lome_lisps = lome_client.parse_story_frames(story_txts)
-with open('tmp-story-frames.lisp', 'w') as f:
-	f.write(lome_lisps)
-print('\tdone (%s)' % (fmt_time(lome_start, time.time())), flush=True)
+	print('\trunning LOME...', flush=True)
+	lome_start = time.time()
+	# Run LOME and output the Lisp frames to a file
+	lome_lisps = lome_client.parse_story_frames(story_txts)
+	with open('tmp-story-frames.lisp', 'w') as f:
+		f.write(lome_lisps)
+	print('\tdone (%s)' % (fmt_time(lome_start, time.time())), flush=True)
 
-# Run parse-frames.lisp to get EL schemas
-print('\n\tconverting frames into schemas...', flush=True)
-conversion_start = time.time()
-lome_to_el_output = subprocess.run('sbcl --script parse-frames.lisp'.split(' '), capture_output=True).stdout.decode('utf-8')
-with open('lome_to_el_output.txt', 'w') as f:
-	f.write(lome_to_el_output)
+	# Run parse-frames.lisp to get EL schemas
+	print('\n\tconverting frames into schemas...', flush=True)
+	conversion_start = time.time()
+	lome_to_el_output = subprocess.run('sbcl --script parse-frames.lisp'.split(' '), capture_output=True, timeout=600).stdout.decode('utf-8')
+	with open('lome_to_el_output.txt', 'w') as f:
+		f.write(lome_to_el_output)
 
-# Run extract_composites to get the actual composites and protoschemas out
-compo_and_proto_lisp = extract_compos(lome_to_el_output.split('\n'), include_protos=True)
-print('\tdone (%s)' % (fmt_time(conversion_start, time.time())), flush=True)
+	# Run extract_composites to get the actual composites and protoschemas out
+	compo_and_proto_lisp = extract_compos(lome_to_el_output.split('\n'), include_protos=True)
+	print('\tdone (%s)' % (fmt_time(conversion_start, time.time())), flush=True)
 
-# Format each composite schema as a "standalone" schema for generalization
-standalone_txts = []
-i = 0
-# get the biggest standalone schema number and start there
-standalone_nums = []
-for f in os.listdir('prompt-standalones/'):
-	try:
-		if len(f) <= len(US_PROMPT) or f[:len(US_PROMPT)] != US_PROMPT:
+	# Format each composite schema as a "standalone" schema for generalization
+	standalone_txts = []
+	i = 0
+	# get the biggest standalone schema number and start there
+	standalone_nums = []
+	for f in os.listdir('prompt-standalones/'):
+		try:
+			if len(f) <= len(us_prompt) or f[:len(us_prompt)] != us_prompt:
+				continue
+
+			num = int(f.split('.')[0].split('_')[-1])
+			standalone_nums.append(num)
+		except:
 			continue
+	if len(standalone_nums) > 0:
+		i = max(standalone_nums) + 1
+	for compo in extract_compos(lome_to_el_output.split('\n'), include_protos=False, as_list=True):
+		if len(compo.strip()) < 10:
+			continue
+		standalone_txts.append('(%s)' % compo)
+		with open('prompt-standalones/%s_%d.txt' % ('_'.join(prompt.split(' ')), i), 'w') as f:
+			f.write('(%s)' % compo)
+		i += 1
 
-		num = int(f.split('.')[0].split('_')[-1])
-		standalone_nums.append(num)
-	except:
-		continue
-if len(standalone_nums) > 0:
-	i = max(standalone_nums) + 1
-for compo in extract_compos(lome_to_el_output.split('\n'), include_protos=False, as_list=True):
-	standalone_txts.append('(%s)' % compo)
-	with open('prompt-standalones/%s_%d.txt' % ('_'.join(PROMPT.split(' ')), i), 'w') as f:
-		f.write('(%s)' % compo)
-	i += 1
+	return i
 
-# Create a general schema
-subprocess.run(('bash schema-gen.sh %s' % '_'.join(PROMPT.split(' '))).split(' '), capture_output=True)
+	# Create a general schema
+	# subprocess.run(('bash schema-gen.sh %s' % '_'.join(prompt.split(' '))).split(' '), capture_output=True)
+
+def loop_gen(prompt, total_num=15, chunk_size=5):
+	us_prompt = '_'.join(prompt.split(' '))
+
+	while True:
+		# Find out how many stories we need to generate
+		num_already = 0
+		for f in os.listdir('prompt-standalones/'):
+			try:
+				if len(f) <= len(US_PROMPT) or f[:len(US_PROMPT)] != US_PROMPT:
+					continue
+				num_already += 1
+			except:
+				continue
+		need_to_make = total_num - num_already
+		if need_to_make <= 0:
+			break
+		print('made %d out of %d' % (num_already, total_num))
+
+		# Generate them, either chunk_size at a time, or, if we need N<chunk_size, N at a time
+		if need_to_make >= chunk_size:
+			print('trying to make %d more' % chunk_size)
+			gen(prompt, chunk_size)
+		else:
+			print('trying to make %d more' % (need_to_make))
+			gen(prompt, need_to_make)
+
+loop_gen(PROMPT, total_num=NUM_STORIES, chunk_size=5)
 
 '''
 # Append all of the new extracted schemas to nesl-compos.txt, then
